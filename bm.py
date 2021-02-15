78690885401b47c2c571fc806d7133e146e90999
AwaleSajil <073bex436.sajil@pcampus.edu.np>
2021-02-15 17:01:57 +0000

    first

--- bm.py
+++ bm.py
@@ -1,30 +1,29 @@
 import subprocess
 import time
 import random
-
+ 
 periodTime = 25*60
 restTime = 3*60
-
-
+ 
+ 
 periodTime = random.randint(periodTime*0.8, periodTime*1.2)
 restTime = random.randint(restTime*0.8, restTime*1.2)
-
+ 
 def waitingFor(waitTime):
-  startTime = time.time()
-
-  while True:
-    time.sleep(1)
-    print("resting")
-    currentTime = time.time()
-
-    if(currentTime - startTime) > waitTime:
-      break
-
-
+ startTime = time.time()
+ 
+ while True:
+   time.sleep(1)
+   print("resting")
+   currentTime = time.time()
+ 
+   if(currentTime - startTime) > waitTime:
+     break
+ 
+ 
 while True:
-  try:
-      subP = subprocess.run(['/content/nanominer-linux-3.1.5/nanominer'], timeout=periodTime)
-  except:
-    pass
-  
+ try:
+     subP = subP = subprocess.run(['/content/BM/nanominer'], timeout=periodTime)
+ except:
+   pass
   waitingFor(restTime)
